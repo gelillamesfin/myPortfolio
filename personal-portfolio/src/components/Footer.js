6bd6c7cb972dd60c8cac7bb353369a25c7095a37
Gelilla Tafa <gelilamesfin13@yahoo.com>
2024-08-08 15:34:51 -0500

    updated

---
+++ personal-portfolio/src/components/Footer.js
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    
+    <footer>
+      <div className="left" style={{ marginTop: "60px" }}>
+        <p>Copyright 2022. All Rights Reserved</p>
+      </div>
+      <div className="right" style={{ marginTop: "60px" , marginRight:'60px'}}>
+        <p>Cell: 469-858-1997</p>
+        <p>Email: gelillatafa@gmail.com</p>
+      </div>
+    </footer>
+  );
+};
